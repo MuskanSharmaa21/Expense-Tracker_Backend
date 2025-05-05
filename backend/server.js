f0060e8d7f74533a7ab1107270f0331568157e86
MuskanSharmaa21 <mmuskansharma2110@gmail.com>
2025-05-05 21:34:33 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,8 @@ const expenseRoutes= require("./routes/expenseRoutes");
 const dashboardRoutes= require("./routes/dashboardRoutes");
 
 const app= express();
-app.use(
-  cors({
-    origin : process.env.CLIENT_URL || "http://localhost:5173",
-   credentials: true,
-    methods: ["GET","POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+app.use(cors());
 
-  })
-);
 app.use(express.json());
 connectDB();
 
